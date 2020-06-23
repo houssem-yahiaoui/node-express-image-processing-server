f28833801d0c89bc5f20e4e9ac1bbcf4519716bf
Houssam Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2020-06-23 15:54:08 +0100

    more multer code

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const router = require('./src/router');
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/', router);
+app.use(express.static(__dirname, 'upload'));
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex);
 });
